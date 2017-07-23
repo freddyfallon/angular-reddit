08b695ecb0e1a3df5047b895636fda29a539d14d
Freddy Fallon <freddyfallon@gmail.com>
2017-07-23 10:49:45 +0100

    Add ability to show multiple articles

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Article } from './article.model';
 
 @Component({
@@ -10,14 +10,9 @@ import { Article } from './article.model';
   }
 })
 export class ArticleComponent implements OnInit {
-  article: Article;
+  @Input() article: Article;
 
   constructor() {
-    this.article = new Article(
-      'Angular 2',
-      'http://www.angular.io/',
-      10
-    )
   }
 
   voteUp() {
